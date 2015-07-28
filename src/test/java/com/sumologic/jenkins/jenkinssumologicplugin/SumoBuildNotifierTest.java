b48c4fa28f90485c08e9a3767285f1d45019fb88
Deven Norton <deven@sumologic.com>
2015-07-28 10:21:58 -0700

    Lets start testing what we have

---
+++ src/test/java/com/sumologic/jenkins/jenkinssumologicplugin/SumoBuildNotifierTest.java
@@ -0,0 +1,25 @@
+package com.sumologic.jenkins.jenkinssumologicplugin;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SumoBuildNotifierTest {
+
+  @Before
+  public void setUp() throws Exception {
+
+  }
+
+  @After
+  public void tearDown() throws Exception {
+
+  }
+
+  @Test
+  public void testSend() throws Exception {
+
+  }
+}
